d06b9328e05157c6f21fb0e882ef62ef6af015ca
sweety225 <36558121+sweety225@users.noreply.github.com>
2018-02-17 12:03:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,15 +11,15 @@ app.get('/', function (req, res) {
 
 
 app.get('article-one', function (req, res) {
-  res.sendF('article-one will be served');
+  res.sendFile('article-one will be served');
 });
 
 app.get('article-two', function (req, res) {
-  res.sendF('article-one will be served');
+  res.sendFile('article-one will be served');
 });
 
 app.get('article-three', function (req, res) {
-  res.sendF('article one will be served');
+  res.sendFile('article one will be served');
 });
 
 app.get('/ui/madi.png', function (req, res) {
